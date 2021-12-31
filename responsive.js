ea42093cfe0f52c41a27c852f00a761e438624c6
nhat2012782 <gonbbb1@gmail.com>
2021-12-31 22:20:34 +0700

    commit on responsive.js

--- responsive.js
+++ responsive.js
@@ -5,48 +5,41 @@ if (width<740) {
     var sMenuButton=document.querySelector('.header-menu')
 
     var sSubnav=document.querySelectorAll('.s-subnav')
+    
+    console.log(sSubnav)
 
-    // function sSubNavHidden() {
-    //     if(sSubnav[0].style.display == "none") {
-    //         for (var i=0;i<sSubnav.length;i++) {
-    //             sSubnav[i].style.cssText = "display: block !important;";
-    //         }
-    //     }
-    //     else {
-    //         for (var i=0;i<sSubnav.length;i++) {
-    //             sSubnav[i].style.cssText = "display: none !important;";
-    //         }
-    //     }
-    // }
-    var moreBtn=document.querySelector('.preventDefault')
-    console.log(moreBtn)
-    for (var i=0;i<sSubnav.length-1;i++) {
-        sSubnav[i].onclick =function(e) {
-            if(sSubnav[0].style.display == "none") {
-                for (var i=0;i<sSubnav.length;i++) {
-                    sSubnav[i].style.cssText = "display: block !important;";
-                }
-            }
-            else {
-                for (var i=0;i<sSubnav.length;i++) {
-                    sSubnav[i].style.cssText = "display: none !important;";
-                }
-            }
-        }
-    }
-
-
-    sMenuButton.onclick=function(e) {
-        if(sSubnav[0].style.display == "none") {
+    function sSubNavHidden() {
+        if(sSubnav[0].classList.contains("s-subnav")) {
             for (var i=0;i<sSubnav.length;i++) {
-                sSubnav[i].style.cssText = "display: block !important;";
+                sSubnav[i].classList.toggle("s-subnav")
             }
         }
         else {
             for (var i=0;i<sSubnav.length;i++) {
-                sSubnav[i].style.cssText = "display: none !important;";
+                sSubnav[i].classList.toggle("s-subnav")
             }
         }
     }
+
+    sMenuButton.onclick=sSubNavHidden;
+
+    var moreBtn=document.querySelector('.preventDefault')
+
+    // console.log(moreBtn)
+
+    // for (var i=0;i<sSubnav.length-1;i++) {
+    //     sSubnav[i].onclick =function(e) {
+    //         if(sSubnav[0].style.display == "none") {
+    //             for (var i=0;i<sSubnav.length;i++) {
+    //                 sSubnav[i].style.cssText = "display: block !important;";
+    //             }
+    //         }
+    //         else {
+    //             for (var i=0;i<sSubnav.length;i++) {
+    //                 sSubnav[i].style.cssText = "display: none !important;";
+    //             }
+    //         }
+    //     }
+    // }
 
 }
